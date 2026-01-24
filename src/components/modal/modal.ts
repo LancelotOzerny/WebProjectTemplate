6499852c56b7539be2a07b0731b910df9bdcda6b
LancelotOzerny <lancelot.ozernuy@gmail.com>
2026-01-25 02:08:45 +0300

    Реализовал открытие и закрытие модальных окон, инициализацию всех модальных окон при загрузке страницы

---
+++ src/components/modal/modal.ts
@@ -0,0 +1,66 @@
+export class ModalWindow
+{
+    private modal : HTMLElement | null = null;
+
+    constructor(private modalName : string)
+    {
+        this.modal = document.querySelector('*[data-modal-name="' + this.modalName + '"]')
+
+        this.setStyles();
+        this.setOpenButtons();
+        this.setCloseButtons();
+    }
+
+    setStyles(): void
+    {
+        if (!this.modal)
+        {
+            return;
+        }
+
+        this.modal.style.left = -(this.modal.clientWidth + 10) + 'px';
+    }
+
+    setCloseButtons(): void
+    {
+        let buttons: NodeListOf<Element> = document.querySelectorAll('*[data-modal-close="' + this.modalName + '"]');
+        buttons.forEach(btn => btn.addEventListener('click', this.close.bind(this)));
+    }
+
+    setOpenButtons(): void
+    {
+        let buttons: NodeListOf<Element> = document.querySelectorAll('*[data-modal-open="' + this.modalName + '"]');
+        buttons.forEach(btn => btn.addEventListener('click', this.open.bind(this)));
+    }
+
+    open(): void
+    {
+        if (this.modal === null)
+        {
+            return;
+        }
+
+        if (!this.modal.parentElement?.classList.contains('active'))
+        {
+            this.modal.parentElement?.classList.add('active');
+        }
+
+        this.modal.style.left = 'calc(50% - ' + this.modal.clientWidth / 2 + 'px )'
+        this.modal.style.top = 'calc(50% - ' + this.modal.clientHeight / 2 + 'px )'
+    }
+
+    close(): void
+    {
+        if (this.modal === null)
+        {
+            return;
+        }
+
+        if (this.modal.parentElement?.classList.contains('active'))
+        {
+            this.modal.parentElement?.classList.remove('active');
+        }
+
+        this.modal.style.left = -(this.modal.clientWidth + 10) + 'px';
+    }
+}
--- src/typescript/compile/script.ts
+++ src/typescript/compile/script.ts
@@ -1,3 +1,22 @@
+import { ModalWindow } from "@components/modal/modal.js";
+
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('script is ready');
+    initModalBindings();
 });
+
+function initModalBindings()
+{
+    const modals = document.querySelectorAll('[data-modal-name]');
+
+    modals.forEach(item => {
+        const name = item.getAttribute('data-modal-name');
+        if (!name) {
+            console.warn(`Модальное окно без data-modal-name`, item);
+            return;
+        }
+
+        let modal : ModalWindow = new ModalWindow(name);
+    });
+
+    console.log('Модальные окна инициализированы:', modals.length);
+}
